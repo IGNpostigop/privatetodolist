92dc9010046b59f612aaaaa26413a3329623beae
Ignacio Postigo Pérez <ignpostigop@gmail.com>
2023-09-22 18:35:14 +0200

    fix

--- script.js
+++ script.js
@@ -1,57 +1,121 @@
+// Arreglo para almacenar las tareas
+let tasks = [];
+
+// Configuración de Firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyCIzlvLRjBi-HIwc_UsgOu36PV3DJ0bB3M",
+    authDomain: "privatetodolist-a9e6d.firebaseapp.com",
+    databaseURL:"https://privatetodolist-a9e6d-default-rtdb.firebaseio.com/",
+    projectId: "privatetodolist-a9e6d",
+    storageBucket: "privatetodolist-a9e6d.appspot.com",
+    messagingSenderId: "558228158361",
+    appId: "1:558228158361:web:4d2962c69584b25ba68508",
+    measurementId: "G-VDRSBEZJNS"
+};
+
+// Inicializar Firebase
+firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
+const tasksRef = database.ref("tasks");
+
 document.addEventListener("DOMContentLoaded", function () {
-    const taskList = document.getElementById("taskList");
+    const taskInput = document.getElementById("taskInput");
+    const addTaskButton = document.getElementById("addTaskButton");
 
-    // Función para agregar una nueva tarea con color de fondo
-    function addTask(task) {
-        const newTaskElement = document.createElement("li");
-        newTaskElement.textContent = task.title;
-
-        // Verifica si la tarea tiene un color de fondo definido
-        if (task.backgroundColor) {
-            newTaskElement.style.backgroundColor = task.backgroundColor;
+    // Agregar tarea al presionar Enter
+    taskInput.addEventListener("keyup", function (event) {
+        if (event.key === "Enter") {
+            addTask();
         }
+    });
 
-        // Agrega botones para eliminar y editar la tarea
+    // Agregar tarea al hacer clic en el botón
+    addTaskButton.addEventListener("click", addTask);
+
+    // Resto del código como antes...
+});
+
+// Función para cargar las tareas desde Firebase al cargar la página
+function loadTasks() {
+    tasksRef.on("value", (snapshot) => {
+        tasks = [];
+        snapshot.forEach((childSnapshot) => {
+            tasks.push(childSnapshot.val());
+        });
+        updateTaskList();
+    });
+}
+
+// Función para guardar las tareas en Firebase
+function saveTasks() {
+    tasksRef.set(tasks);
+}
+
+function addTask() {
+    const taskInput = document.getElementById("taskInput");
+    const taskText = taskInput.value.trim();
+
+    if (taskText !== "") {
+        // Agregar la tarea a Firebase
+        tasks.push(taskText);
+        saveTasks();
+
+        // Actualizar la lista de tareas
+        updateTaskList();
+
+        // Limpiar el campo de entrada
+        taskInput.value = "";
+    }
+}
+
+function updateTaskList() {
+    const taskList = document.getElementById("taskList");
+    taskList.innerHTML = ""; // Limpiar la lista antes de actualizar
+
+    // Recorrer el arreglo de tareas y mostrarlas en la lista
+    for (let i = 0; i < tasks.length; i++) {
+        const taskText = tasks[i];
+
+        // Crear elementos HTML para mostrar la tarea
+        const li = document.createElement("li");
+        li.textContent = taskText;
+
+        // Crear botones para eliminar y editar la tarea
         const deleteButton = document.createElement("button");
         deleteButton.textContent = "Eliminar";
-        deleteButton.className = "delete-button";
+        deleteButton.addEventListener("click", () => deleteTask(i));
 
         const editButton = document.createElement("button");
         editButton.textContent = "Editar";
-        editButton.className = "edit-button";
+        editButton.addEventListener("click", () => editTask(i));
 
-        newTaskElement.appendChild(deleteButton);
-        newTaskElement.appendChild(editButton);
-
-        taskList.appendChild(newTaskElement);
+        // Agregar elementos a la lista
+        li.appendChild(deleteButton);
+        li.appendChild(editButton);
+        taskList.appendChild(li);
     }
+}
 
-    // Ejemplo de tareas con colores de fondo
-    const tasks = [
-        { title: "Tarea 1", backgroundColor: "#ff5733" }, // Ejemplo de tarea con fondo rojo
-        { title: "Tarea 2", backgroundColor: "#33ff57" }, // Ejemplo de tarea con fondo verde
-        // ... Agrega más tareas aquí
-    ];
+function deleteTask(index) {
+    // Eliminar la tarea del arreglo
+    tasks.splice(index, 1);
+    saveTasks();
 
-    // Agrega las tareas a la lista
-    tasks.forEach(addTask);
+    // Actualizar la lista de tareas
+    updateTaskList();
+}
 
-    // Evento para eliminar una tarea al hacer clic en el botón "Eliminar"
-    taskList.addEventListener("click", function (event) {
-        if (event.target.classList.contains("delete-button")) {
-            event.target.parentElement.remove();
-        }
-    });
+function editTask(index) {
+    const newTaskText = prompt("Editar tarea:", tasks[index]);
+    
+    if (newTaskText !== null) {
+        tasks[index] = newTaskText;
+        saveTasks();
 
-    // Evento para editar una tarea al hacer clic en el botón "Editar"
-    taskList.addEventListener("click", function (event) {
-        if (event.target.classList.contains("edit-button")) {
-            const taskText = event.target.parentElement.textContent;
-            const newTaskText = prompt("Editar tarea:", taskText);
+        // Actualizar la lista de tareas
+        updateTaskList();
+    }
+}
 
-            if (newTaskText !== null) {
-                event.target.parentElement.textContent = newTaskText;
-            }
-        }
-    });
-});
+// Llamar a loadTasks al cargar la página para cargar las tareas existentes desde Firebase
+loadTasks();
